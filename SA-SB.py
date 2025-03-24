613d0a7c8fb71b848090f477abb097427b73e6ff
Binula Kavisinghe <bkavisinghe@gmail.com>
2025-03-24 18:32:03 +0530

    Update SA-SB.py

--- SA-SB.py
+++ SA-SB.py
@@ -1,4 +1,5 @@
 v1 = []
+ph_values = []
 for i in range(0,50+1):
     v1.append(round(i*0.001,4))
 print (v1)
@@ -6,7 +7,7 @@ print (v1)
 c1 = c2 = 1.0
 v2 = 0.025
 
-n2 = c1 * v2
+n2 = c2 * v2
  
 
 
@@ -16,17 +17,19 @@ for i in v1:
     n1 = c1 * float(i)
 
     if n1 == 0:
-        ph = -1*math.log(n2 / )
+        ph = 0
 
-    elif n2 < n1:
-        ph = -1 * math.log ((n2 - n1) / ( i + v2)  )
+    elif n2 > n1:
+        ph = -1*math.log ((n2 - n1) / ( float(i) + v2)  )
 
     elif n2 == n1:
         ph = 7
 
-    # else:
-    #     poh = -1 * math.log ((n1 - n2) / (i + v2))
-    #     ph = 14 - poh
+    else:
+        poh = -1 * math.log ((n1 - n2) / (i + v2))
+        ph = 14 - poh
 
-    print(i, "---", round(ph, 4))
+    ph_values.append(round(ph,4))
+
+    print(i, "---", round(ph,4) )
 
