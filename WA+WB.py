725a78b7e194a628bd212ba7a0962d3a4bd78524
Binula Kavisinghe <bkavisinghe@gmail.com>
2025-03-30 00:35:09 +0530

    Update WA+WB.py

--- WA+WB.py
+++ WA+WB.py
@@ -13,6 +13,8 @@ kb = 0.00002
 
 n2 = c2 * v2 
 
+import math
+
 for i in v1:
     n1 = c1 * float(i)
 
@@ -21,3 +23,18 @@ for i in v1:
     finaln2 = (c2 * ka) ** 0.5
 
     print(i, " -- ", finaln1, "  ", finaln2 )
+
+    if finaln2 > finaln1:
+        ph = -1 * math.log((finaln2 - finaln1) / (float(i + v2)))
+
+    elif finaln2 < finaln1:
+        poh = -1 * math.log((finaln1 - finaln2) / (float(i + v2)))
+        ph = 14 - poh
+
+    elif finaln2 == finaln1:
+        ph = 0000000000
+
+    else:
+        ph = -1 * math.log(ka ** 0.5)
+
+    print (i, "---", ph)
