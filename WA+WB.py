f91d0750d1a072b85a124b40086fed02eeed16a2
Binula Kavisinghe <bkavisinghe@gmail.com>
2025-04-01 00:30:51 +0530

    Testing

--- WA+WB.py
+++ WA+WB.py
@@ -2,13 +2,13 @@ v1 = []
 ph_values = []
 for i in range(0,50+1):
     v1.append(round(i*0.001,4))
-print (v1)
+#print (v1)
 
 c1 = c2 = 1.0
 v2 = 0.025
 
-ka = 2 * (10**-5)
-kb = 2 * (10**-5)
+ka = 1.75 * (10**-5)
+kb = 1.76 * (10**-5)
 
 
 n2 = c2 * v2 
@@ -17,21 +17,25 @@ import math
 
 for i in v1:
     n1 = c1 * float(i)
+    vtotal = i + v2
 
     finaln1 = ((c1 * kb) ** 0.5)*i
-
     finaln2 = ((c2 * ka) ** 0.5)*v2
 
     #print(i, " -- ", finaln1, "  ", finaln2 )
 
     if finaln1 == 0:
-        ph = -1 * math.log(finaln2)
+        ph = -1 * math.log(finaln2 / vtotal)
     
     elif finaln2 > finaln1:
-        ph = math.log(ka) + math.log((finaln2 / (n2 - finaln2)) / (i + v2))
+        saltconc = finaln2 / vtotal
+        exacidconc = (n2 - finaln2) / vtotal
+        ph = -1*math.log(ka) + math.log(saltconc / exacidconc)
 
     elif finaln2 < finaln1:
-        poh = math.log(kb) + math.log((finaln1 / (n1 - finaln1)) / (i + v2))
+        saltconc = finaln1 / vtotal
+        exbaseconc = (n1 - finaln1) / vtotal
+        poh = -1*math.log(kb) + math.log(saltconc / exbaseconc)
         ph = 14 - poh
 
     else:
